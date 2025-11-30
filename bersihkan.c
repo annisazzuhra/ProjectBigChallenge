dc815dbb8bd92ada32246324684b43a772c19e9b
annisazzuhra <azzuhra25@mhs.usk.ac.id>
2025-11-30 14:10:54 +0700

    p

--- bersihkan.c
+++ bersihkan.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+void bersihkan_file(const char *input, const char *output) {
+    FILE *fp = fopen(input, "r");
+    FILE *fw = fopen(output, "w");
+
+    if (fp == NULL || fw == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    int c;
+    while ((c = fgetc(fp)) != EOF) {
+
+        // karakter yang boleh hanyalah huruf besar dan kecil + spasi
+        if ((c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            c == ' ' || c == '\n') {
+
+            fputc(c, fw);
+
+        } else {
+            fputc(' ', fw);   // karakter lain diganti spasi
+        }
+    }
+
+    fclose(fp);
+    fclose(fw);
+}
 
